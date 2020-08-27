7c85fefde893849a04c5ea99ef8c255fa79cdee1
Maloochow <luyao.mly@gmail.com>
2020-08-27 17:18:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    const cards = []
+    for (let i = 0; i < array.length; i++ ) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return cards
+}
+
+function countDown(num) {
+    let i = num
+    while (i >= 0) {
+        console.log(i)
+        i = i-1
+    }
+}
